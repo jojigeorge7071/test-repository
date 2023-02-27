023a90ad3c63eadcfd24a203f1167da6d1bb90cd
jojigeorge <gjojigeorge@gmail.com>
2023-02-27 10:42:07 +0530

    add a console.log in version-2

--- helloworld.js
+++ helloworld.js
@@ -3,5 +3,6 @@ const http =require('http')
 const myServer = http.createServer((req,res)=>{
     console.log('New request recived');
     res.end('God is Love')
+    res.end('version-2')
 })
 myServer.listen(5000,()=>{console.log("server started 5000")})
