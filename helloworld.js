fbd0a687f01ea6076a03a307b9feef1ea7d31454
jojigeorge <gjojigeorge@gmail.com>
2023-02-27 10:46:39 +0530

    add a console in master

--- helloworld.js
+++ helloworld.js
@@ -2,7 +2,8 @@ const http =require('http')
 
 const myServer = http.createServer((req,res)=>{
     console.log('New request recived');
-    res.end('God is Love \nEdit after first commit')
+    console.log('add console in master');
+    res.end('God is Love ')
 
 })
 myServer.listen(5000,()=>{console.log("server started 5000")})
